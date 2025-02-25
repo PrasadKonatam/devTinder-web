0323971c6aa48533036c9efe90e8e6d2cc596319
Prasad Konatam <prasadkonatam96@gmail.com>
2025-02-25 12:00:04 +0530

    git initialized

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className="text-amber-300">Hello</h1>
+      <h2 className='text-lg'>welcome back Prasad Konatam</h2>
+    </>
+  )
+}
+
+export default App
