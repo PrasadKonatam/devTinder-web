758e0971d82a176b2a597c847fe8f65e8164af89
Prasad Konatam <prasadkonatam96@gmail.com>
2025-02-28 15:56:20 +0530

    added checks for accessing without logging in

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NavBar from "./NavBar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import Footer from "./Footer";
+import { useDispatch } from "react-redux";
+import axios from "axios";
+import base_url from "../utils/constants";
+import { addUser } from "../utils/userSlice";
 
 const Body = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const fetchUser = async () => {
+    try {
+      const user = await axios.get(base_url + "/profile/view", {
+        withCredentials: true,
+      });
+      dispatch(addUser(user.data));
+    } catch (error) {
+      if (error.status === 400) {
+        return navigate("/login");
+      }
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
   return (
     <>
       <NavBar />
